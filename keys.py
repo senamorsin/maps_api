18e66a867bfe24653131375c2084d2a07ceb3f85
senamorsin <senamorsin@yandex.ru>
2026-02-14 01:18:51 +0300

    Наш личный ключ для статик апи

--- keys.py
+++ keys.py
@@ -1,3 +1,3 @@
-static_key = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+static_key = "fb8daca8-7557-46fc-95f8-7ce188f32b0f"
 geocoder_key = "8013b162-6b42-4997-9691-77b7074026e0"
 geosearch_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
